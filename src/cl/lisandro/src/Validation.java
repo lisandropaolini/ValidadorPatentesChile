9feeca17ac9d066a29e33d5b489f5cc3a371a69c
ChacoSoft <alejandroe.calderon@gmail.com>
2016-12-01 14:49:33 -0300

    Reorganziacion del package. Modificacion del pom para generar el jar.

--- src/cl/lisandro/src/Validation.java
+++
@@ -1,8 +0,0 @@
-package cl.lisandro.src;
-
-public interface Validation {
-
-	public Boolean isValid(String patente);
-	public String setNormalized(String patente);
-	public String setComplete(String patente);
-}
